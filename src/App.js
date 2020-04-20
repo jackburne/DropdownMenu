bf7b70fc72cd9724b73cb7566842329aa707b57c
Jack Burne <jack_burne@yahoo.co.uk>
2020-04-20 20:27:49 +0100

    Tweaked error with Chevron icon (wrong name) & added the dropdown menu component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // Importing all the svg's that we are going to need from inside the ./icons folder
 // We import them as ReactComponent's and name then with CamelCase, we can then
 // use them in a JSX element (for example, to use ArrowIcon, we would do: { <ArrowIcon> } )
@@ -6,10 +6,11 @@ import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
 import { ReactComponent as BellIcon } from './icons/bell.svg';
 import { ReactComponent as BoltIcon } from './icons/bolt.svg';
 import { ReactComponent as CaretIcon } from './icons/caret.svg';
-import { ReactComponent as Chevron } from './icons/chevron.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
 import { ReactComponent as CogIcon } from './icons/cog.svg';
 import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
 import { ReactComponent as PlusIcon } from './icons/plus.svg';
+
 
 // Main function of the app that actually get's displayed on the webpage (As it is the default export)
 function App() {
@@ -26,7 +27,7 @@ function App() {
 
       <NavItem icon={<CaretIcon />}>
 
-
+        <DropdownMenu/>
 
       </NavItem>
 
@@ -46,12 +47,44 @@ function Navbar(props) {
 
 // React component for the individual items, or buttons, that will be on the navbar
 function NavItem(props) {
+
+  // useState returns an array of variables, so we use two const's to hold the two values as seperate variables
+  const [open, setOpen] = useState();
+
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
+
+      {open && props.children}
     </li>
+  )
+}
+
+function DropdownMenu() {
+
+  function DropDownItem(props) {
+    return (
+      <a href="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+        
+        {props.children}
+
+        <span className="icon-right">{props.rightIcon}</span>
+
+      </a>
+    )
+  }
+
+  return (
+    <div className="dropdown">
+      <DropDownItem>My Profile</DropDownItem>
+      <DropDownItem
+        leftIcon={<CogIcon />}
+        rightIcon={<ChevronIcon />}>
+      </DropDownItem>
+    </div>
   )
 }
 
