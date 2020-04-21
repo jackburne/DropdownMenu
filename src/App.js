3c4c4c2fa72b68174492977b5c3b5b39fc733d65
Jack Burne <jack_burne@yahoo.co.uk>
2020-04-21 01:21:00 +0100

    Fixed dropdown animations to be smooth and calc height

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 // Importing all the svg's that we are going to need from inside the ./icons folder
 // We import them as ReactComponent's and name then with CamelCase, we can then
@@ -66,31 +66,70 @@ function NavItem(props) {
 function DropdownMenu() {
 
   const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight)
+  }, [])  
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
   // As we want this to be a dropdown menu with items within it, we declare the Dropdown menu item component
   // as a component WITHIN the dropdown menu itself
   function DropDownItem(props) {
     return (
-      <a href="#" className="menu-item">
+      <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
         <span className="icon-button">{props.leftIcon}</span>
-        
         {props.children}
-
         <span className="icon-right">{props.rightIcon}</span>
-
       </a>
     )
   }
 
   // After declaring the dropdownItem component we can return a div with as dropdownItems we want within it
   return (
-    <div className="dropdown">
+    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
       <CSSTransition 
         in={activeMenu === 'main'}
-        unmountOnExit timeout={500}
+        timeout={500}
+        unmountOnExit 
         classNames="menu-primary"
-        >
+        onEnter={calcHeight}>
           <div className="menu">
             <DropDownItem>My Profile</DropDownItem>
+            <DropDownItem
+              leftIcon={<CogIcon />}
+              rightIcon={<ChevronIcon />}
+              goToMenu="settings">
+              Settings
+            </DropDownItem>
+            <DropDownItem
+            leftIcon="🦧"
+            rightIcon={<ChevronIcon />}
+            goToMenu="animals">
+            Animals
+          </DropDownItem>
+          </div>
+      </CSSTransition>
+
+      <CSSTransition 
+        in={activeMenu === 'settings'}
+        timeout={500}
+        unmountOnExit
+        onEnter={calcHeight}
+        classNames="menu-secondary"
+        >
+          <div className="menu">
+            <DropDownItem leftIcon={<ArrowIcon />} goToMenu="main" />
+              <h2>Tutorial Menu</h2>
+            <DropDownItem leftIcon={<BoltIcon />}>HTML</DropDownItem>
+            <DropDownItem leftIcon={<BoltIcon />}>Css</DropDownItem>
+            <DropDownItem leftIcon={<BoltIcon />}>JS</DropDownItem>
+            <DropDownItem leftIcon={<BoltIcon />}>Other Shite</DropDownItem>
             <DropDownItem
              leftIcon={<CogIcon />}
              rightIcon={<ChevronIcon />}>
